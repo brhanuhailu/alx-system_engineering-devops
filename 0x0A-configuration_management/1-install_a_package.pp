cc27aa28d687c7c9867bd324e9b48bd2007a5b4b
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-12-09 10:53:27 -0800

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Install the puppet-lint package
-package { 'puppet-lint':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3'
 }
